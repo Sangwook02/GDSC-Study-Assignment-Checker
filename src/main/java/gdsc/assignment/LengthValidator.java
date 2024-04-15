e9f302f5a5ad020868e5e712b59a3d1930c9ef0f
chosw1002@naver.com <chosw1002@naver.com>
2024-04-15 21:19:17 +0900

    feat: wil 글자수에서 url을 제외하는 기능 추가

--- src/main/java/gdsc/assignment/LengthValidator.java
+++ src/main/java/gdsc/assignment/LengthValidator.java
@@ -4,4 +4,9 @@ import java.util.stream.Stream;
 
 public interface LengthValidator {
 	AssignmentStatus validateWilLength(Stream<String> lines);
+
+	default int getWilLength(String content) {
+		content = content.replaceAll("(http|https)://[^\\s]+", "");
+		return content.length();
+	}
 }
